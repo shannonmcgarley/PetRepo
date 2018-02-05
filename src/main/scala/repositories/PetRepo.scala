07e4a07f017b9389ac67092a2fd7ba7fdeb3f70f
shannon mcgarley <shannon.mcgarley@digital.hmrc.gov.uk>
2018-02-05 14:30:07 +0000

    finished adding petrepo methods

--- src/main/scala/repositories/PetRepo.scala
+++ src/main/scala/repositories/PetRepo.scala
@@ -8,12 +8,8 @@ class PetRepo {
   def dogs() : List[Pet] = Nil
   def cats() : List[Pet] = Nil
   def other() : List[Pet] = Nil
-
-
-
-
-
-
-
+  def add(pet : Pet*) : List[Pet]
+  def removeByName (name : String) : List[Pet]
+  def update (pet : Pet) : List[Pet]
 
 }
