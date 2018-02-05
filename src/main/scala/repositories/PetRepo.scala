aabdb1a59e15a68297b1a60136b6849f1df0d075
shannon mcgarley <shannon.mcgarley@digital.hmrc.gov.uk>
2018-02-05 14:25:58 +0000

    start creating repo class

---
+++ src/main/scala/repositories/PetRepo.scala
@@ -0,0 +1,19 @@
+package repositories
+
+class PetRepo {
+
+
+  def all() : List[Pet] = Nil
+  def findByName (name : String) : Option[Pet] = None
+  def dogs() : List[Pet] = Nil
+  def cats() : List[Pet] = Nil
+  def other() : List[Pet] = Nil
+
+
+
+
+
+
+
+
+}
